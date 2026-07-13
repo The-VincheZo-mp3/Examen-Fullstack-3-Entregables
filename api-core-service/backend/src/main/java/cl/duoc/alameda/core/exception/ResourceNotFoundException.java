3c93f167c9232d9a53269ec7133e8d7bca7824d2
The-VincheZo-mp3 <vi.sanchezm@duocuc.cl>
2026-07-12 21:41:34 -0400

    Delete api-core-service directory

--- api-core-service/backend/src/main/java/cl/duoc/alameda/core/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.alameda.core.exception;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) { super(message); }
-}
